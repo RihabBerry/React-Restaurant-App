58914b6806e99428ed0aeda688926701301fea0d
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-27 01:03:27 +0200

    Added AddItem functionality to the modal

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import classes from "./App.module.css";
 import NavBar from "./Component/NavBar/NavBar";
 import List from "./Component/List/List";
 import Modal from "./Component/UI/Modal";
+
 function App() {
   const [showModal, setShowModal] = useState(false);
+  const [shoppingList, setShoppingList] = useState([]);
 
   const showModalHandler = () => {
     setShowModal(!showModal);
   };
+  const addItemToShopingList = (item) => {
+    setShoppingList(shoppingList.concat(item));
+    console.log("this is your list", shoppingList);
+  };
 
+  const removeItemFromShoppingList = (item) => {
+    console.log("removed!!");
+  };
   return (
     <div>
-      <NavBar showModal={showModal} showModalHandler={showModalHandler} />
+      <NavBar
+        showModal={showModal}
+        showModalHandler={showModalHandler}
+        countOfItems={shoppingList.length}
+      />
       <section className={classes.cover}>
         <div className={classes.banner}>
           <h2> Delicous Food,Delivered to you </h2>
@@ -24,9 +37,14 @@ function App() {
           </p>
         </div>
         {showModal && (
-          <Modal showModal={showModal} showModalHandler={showModalHandler} />
+          <Modal
+            shoppingList={shoppingList}
+            showModal={showModal}
+            showModalHandler={showModalHandler}
+            addItemToShopingList={addItemToShopingList}
+          />
         )}
-        <List />
+        <List addItemToShopingList={addItemToShopingList} />
       </section>
     </div>
   );
