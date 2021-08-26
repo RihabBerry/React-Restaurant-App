1126fae0d3383fc077f94ced25b88c44a481e48a
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-27 01:30:41 +0200

    Added groupby functionality

--- src/Component/UI/Modal.js
+++ src/Component/UI/Modal.js
@@ -3,45 +3,47 @@ import ItemToBuy from "../ItemTobuy/ItemToBuy";
 import classes from "./Modal.module.css";
 
 const Modal = (props) => {
-  const list = [
-    {
-      MealName: "Sushi",
-      MealDescription: "Finest fish and Veggies",
-      Price: "$22.99",
-      qty: 2,
-    },
-    {
-      MealName: "Sushi",
-      MealDescription: "Finest fish and Veggies",
-      Price: "$22.99",
-      qty: 2,
-    },
-    {
-      MealName: "Sushi",
-      MealDescription: "Finest fish and Veggies",
-      Price: "$22.99",
-      qty: 2,
-    },
-    {
-      MealName: "Sushi",
-      MealDescription: "Finest fish and Veggies",
-      Price: "$22.99",
-      qty: 2,
-    },
-  ];
+  const groupedBy = (array, key) => {
+    return array.reduce((result, currentValue) => {
+      (result[currentValue[key]] = result[currentValue[key]] || []).push(
+        currentValue
+      );
+      return result;
+    }, {});
+  };
+
+  var totalPrice = props.shoppingList.reduce(function (prev, cur) {
+    return prev + cur.Price;
+  }, 0);
+
+  console.log("this is total price", totalPrice);
+
+  let itemGrouped = groupedBy(props.shoppingList, "MealName");
+
+  const itemsToBuy = Object.keys(itemGrouped).map((item) => {
+    let sum = (itemGrouped[item][0].Price * itemGrouped[item].length).toFixed(
+      2
+    );
+
+    return (
+      <ItemToBuy
+        key={itemGrouped[item][0].id}
+        item={itemGrouped[item][0]}
+        sum={sum}
+        multiply={itemGrouped[item].length}
+        addItemToShopingList={props.addItemToShopingList}
+      />
+    );
+  });
 
   return (
-    <div onClick={props.showModalHandler} className={classes.backdrop}>
+    <div className={classes.backdrop}>
       <div className={classes.wrapper}>
-        <main>
-          <ItemToBuy />
-          <ItemToBuy />
-          <ItemToBuy />
-        </main>
+        <main> {itemsToBuy}</main>
         <footer className={classes.footer}>
           <div className={classes.totalAmount}>
             <h3>Total Amount </h3>
-            <h3> $88.99</h3>
+            <h3> ${totalPrice.toFixed(2)}</h3>
           </div>
           <button className={classes.btnOrder}>Order</button>
           <button className={classes.btnClose} onClick={props.showModalHandler}>
