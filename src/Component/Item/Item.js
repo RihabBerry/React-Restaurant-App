a6761122723b0f754e8401e1b5a1c199757569e3
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-27 01:04:42 +0200

    Read item in item component

--- src/Component/Item/Item.js
+++ src/Component/Item/Item.js
@@ -1,29 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 import classes from "./Item.module.css";
 const Item = (props) => {
+  const addItemtoshoppingList = () => {
+    props.addItemToShopingList(props.item);
+  };
+
   return (
-    <Fragment>
-      <div className={classes.item}>
-        <div>
-          <h3> {props.item.MealName}</h3>
-          <p className={classes.description}>{props.item.MealDescription}</p>
-          <p className={classes.price}>{props.item.Price}</p>
-        </div>
-        <div className={classes.addItem}>
-          <div className={classes.Amount}>
-            Amount:
-            <input
-              type="number"
-              id="quantity"
-              name="quantity"
-              min="0"
-              max="10"
-            />
-          </div>
-          <button className={classes.addBtn}>+Add</button>
-        </div>
+    <div className={classes.item}>
+      <div>
+        <h3> {props.item.MealName}</h3>
+        <p className={classes.description}>{props.item.MealDescription}</p>
+        <p className={classes.price}>{props.item.Price}</p>
       </div>
-    </Fragment>
+      <div className={classes.addItem}>
+        <div className={classes.Amount}>
+          Amount:
+          <input type="number" id="quantity" name="quantity" min="0" max="10" />
+        </div>
+        <button className={classes.addBtn} onClick={addItemtoshoppingList}>
+          +Add
+        </button>
+      </div>
+    </div>
   );
 };
 export default Item;
