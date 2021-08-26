ba2d59cb49c4637f3273bb9817229faa52fb6bcf
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-26 15:32:14 +0200

    Added Container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./App.module.css";
 import NavBar from "./Component/NavBar/NavBar";
 import List from "./Component/List/List";
+import Modal from "./Component/UI/Modal";
 function App() {
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div>
-      <NavBar />
+      <NavBar showModal={showModal} showModalHandler={showModalHandler} />
       <section className={classes.cover}>
         <div className={classes.banner}>
-          <h2> Delicous Food ,Delivered to you </h2>
+          <h2> Delicous Food,Delivered to you </h2>
           <p>
             Choose your Favorite meal from our broad selection of availble meals
             and enjoy a delicious lunch or dinner All our meals are cooked with
-            heigh quality ingredients , just in time and of course by
-            experienced chefs .
+            heigh quality ingredients,just in time and of course by experienced
+            chefs.
           </p>
         </div>
-
+        {showModal && (
+          <Modal showModal={showModal} showModalHandler={showModalHandler} />
+        )}
         <List />
       </section>
     </div>
