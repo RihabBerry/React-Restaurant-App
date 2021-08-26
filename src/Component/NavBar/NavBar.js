d69bc96b52747d27439165466e18112ac3a1b4ae
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-26 15:31:23 +0200

    Added NavBar

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import classes from "./NavBar.module.css";
 
-const NavBar = () => {
+const NavBar = (props) => {
   return (
     <div className={classes.header}>
       <h1>ReactMeals</h1>
       <div className={classes.carte}>
-        <i class="fas fa-shopping-cart"></i>
+        <i class="fas fa-shopping-cart" onClick={props.showModalHandler}></i>
         <h6> Your Cart:</h6>
         <div className={classes.numberOfShops}>0</div>
       </div>
