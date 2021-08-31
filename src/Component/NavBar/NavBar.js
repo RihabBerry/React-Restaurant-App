04a71f64cd5c1ed88f1aaaeec892c06bdd94c7b0
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-08-31 17:06:54 +0200

    used context inside navbar

--- src/Component/NavBar/NavBar.js
+++ src/Component/NavBar/NavBar.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import CartContext from "../../store/cart-context";
 import classes from "./NavBar.module.css";
 
 const NavBar = (props) => {
+  const cartCtx = useContext(CartContext);
+  const numberOfCartItems = cartCtx.shoppingList.reduce(function (prev, cur) {
+    return prev + cur.amount;
+  }, 0);
+
   return (
     <div className={classes.header}>
       <h1>ReactMeals</h1>
       <div className={classes.carte}>
         <i class="fas fa-shopping-cart" onClick={props.showModalHandler}></i>
         <h6> Your Cart:</h6>
-        <div className={classes.numberOfShops}>{props.countOfItems}</div>
+        <div className={classes.numberOfShops}>{numberOfCartItems}</div>
       </div>
     </div>
   );
