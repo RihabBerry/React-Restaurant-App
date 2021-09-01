5f936c3ca5f75358ef76ce576e15e7b59667b4d7
ElrouDeveloper <rihab.berrichiie@gmail.com>
2021-09-01 16:41:14 +0200

    show order button with condition

--- src/Component/Cart/Cart.js
+++ src/Component/Cart/Cart.js
@@ -5,6 +5,8 @@ import classes from "./Cart.module.css";
 import CartContext from "../../store/cart-context";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+
+  const hasItem = cartCtx.shoppingList.length > 0;
   const groupedBy = (array, key) => {
     return array.reduce((result, currentValue) => {
       (result[currentValue[key]] = result[currentValue[key]] || []).push(
@@ -15,16 +17,14 @@ const Cart = (props) => {
   };
 
   console.log("this is isthe shopoinlIST", cartCtx.shoppingList);
-
+  console.log("this is updated totalAMount", cartCtx.totalAmount);
   let itemGrouped = groupedBy(cartCtx.shoppingList, "MealName");
 
-  let totalPrice;
   const itemsToBuy = Object.keys(itemGrouped).map((item) => {
     const multiply = itemGrouped[item].reduce(function (prev, cur) {
       return prev + cur.amount;
     }, 0);
     let sum = (itemGrouped[item][0].Price * multiply).toFixed(2);
-    totalPrice = +sum;
     return (
       <ItemToBuy
         key={itemGrouped[item][0].id}
@@ -44,9 +44,9 @@ const Cart = (props) => {
         <footer className={classes.footer}>
           <div className={classes.totalAmount}>
             <h3>Total Amount </h3>
-            <h3> ${totalPrice}</h3>
+            <h3> ${cartCtx.totalAmount.toFixed(2)}</h3>
           </div>
-          <button className={classes.btnOrder}>Order</button>
+          {hasItem && <button className={classes.btnOrder}>Order</button>}
           <button className={classes.btnClose} onClick={props.showModalHandler}>
             Close
           </button>
